4537c3bb77ab8787f19eddf0613a906c176f946a
Sreekar Garlapati <sreekar.garlapati@gmail.com>
2019-06-03 11:34:25 +0530

    modified to run on my arch

--- parse.py
+++ parse.py
@@ -46,7 +46,6 @@ SAMPLE_OUTPUT='output'
 MY_OUTPUT='my_output'
 
 # Do not modify these!
-VERSION='CodeForces Parser v1.5.1: https://github.com/johnathan79717/codeforces-parser'
 RED_F='\033[31m'
 GREEN_F='\033[32m'
 BOLD='\033[1m'
@@ -216,7 +215,6 @@ def generate_test_script(folder, language, num_tests, problem):
 
 # Main function.
 def main():
-    print (VERSION)
     parser = argparse.ArgumentParser()
     parser.add_argument('--language', '-l', default="c++14", help="The programming language you want to use "
             "(c++14, go)")
